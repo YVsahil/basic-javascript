1e680a4a8ebb2dc58e2fe31af1cc03579fd7438e
Sahil Yadav <sahil.yadav.98078@gmail.com>
2023-01-05 16:27:14 +0545

    learning basic js

---
+++ index.js
@@ -0,0 +1,51 @@
+/*alert("Namaste")
+var x=3;
+console.log(x);
+var x=12;
+console.log(x)
+var x=12*12;
+console.log(x);
+alert(x)*/
+
+// let y=23;
+// console.log(y);
+// y=234;
+// console.log(y);
+// let y=34;
+// console.log(y);
+
+// const PI=3.14;
+// console.log(PI);
+// PI=5678;
+// console.log(PI);
+// const PI=568;
+// console.log(PI);
+
+// const x=90;
+// console.log(x);
+// console.log(typeof x);
+
+// const Name="YV";
+// console.log(Name);
+// console.log(typeof Name);
+
+// let bool=true;
+// console.log(bool);
+// console.log(typeof bool);
+
+// let x=90;
+// let y=56;
+// let z=false;
+// let res=x+y+z;
+// console.log(res);
+
+// let x="abl";
+// let y=40;
+// let find=x+y;
+// console.log(find);
+
+const name="YV";
+const sur="SAHIL";
+console.log("My name is : "+name+" "+sur+".");
+tia=(`MY name is : ${name} ${sur}.`);
+console.log(`Hakumana tata : ${tia}`);
