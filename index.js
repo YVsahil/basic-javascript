ff183b00de55c5cf25562fe4c41c62c9603b1c55
Sahil Yadav <sahil.yadav.98078@gmail.com>
2023-01-09 16:29:07 +0545

    arr

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ alert(x)*/
 // console.log(typeof Number.toString(num));
 
 //string method
- let str="Hello, world, RAM";
-let y="Hakuna matata";
+//  let str="Hello, world, RAM";
+// let y="Hakuna matata";
 
 // //charat
 // // console.log(str.charAt(6));
@@ -79,7 +79,101 @@ let y="Hakuna matata";
 // console.log(str.toLowerCase());
 // console.log(y.toUpperCase());
 
-let z="    hakuna    ma   tata";
-console.log(z)
-console.log(z.trim());
-console.log(str.slice(0,2));
+
+
+// let z="    hakuna    ma   tata";
+// console.log(z)
+// console.log(z.trim());
+// console.log(str.slice(0,2));
+
+// const heading=document.getElementById("a");
+// console.log(heading);
+// heading.innerText="monaotghefdbxgfh";
+
+// int a=1;
+// while(a<28)
+// {
+//     console.log(a);
+//     a++;
+// }
+
+// const num=[1,5,2,7,3,6];
+// console.log(num.length);
+// console.log(num[5]);
+
+//for loop
+// for(x=0;x<=20;x++)
+// {
+//     console.log(x);
+// }
+
+//while loop
+// x=0;
+// while(x<=20)
+// {
+//     console.log(x);
+//     x++;
+// }
+
+//do while
+// do {
+//     x=1;
+//     console.log(x);
+// } while (x>20);
+
+//const myarr=[4,6,2,4,5,3,4,22,3,];
+// console.log(myarr.length);
+
+// const myarr2=["ram","shreya","abnish","rahul"];
+// console.log(myarr2);
+
+// //push
+// const newadd=myarr2.push("Amit");
+// console.log(myarr2);
+
+// //unshift
+// console.log(myarr2.unshift("bablu"));
+// console.log(myarr2);
+
+
+//pop
+// const a=[2,3,4,2,24,2,3,4,];
+// // console.log(a);
+// const b=a.pop();
+// console.log(b);
+
+//shift
+// const a=[2,3,4,9,24,12,33,64,];
+// console.log(a);
+// const b=a.shift();
+// console.log(b);
+// console.log(a);
+
+//concate
+// let a=[2,3,4,5,6];
+// let b=["ram","shyam","hari","uuuu","poute"];
+// console.log(a.concat(b));
+// console.log(a.reverse());
+
+//filter
+// a=[4,5,6,7,8,2,34,345,3498];
+// console.log(a);
+// const findfilter=a.filter(n=>n>30);
+// console.log(findfilter);
+// console.log(a);
+
+//splice 
+// a=[4,5,6,7,8,2,34,345,3498];
+// console.log(a);
+// const b=a.splice(3,4);
+// console.log(b);
+// console.log(a);
+
+// console.log(a.reverse());
+
+//sort
+a=[5243,5,6,17,48,2,34,345,498];
+console.log(a);
+const b=a.sort();
+console.log(b);
+console.log(a);
